b83cb5e731f5e62f763c8bc43d0c262a0f3ade07
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-03-13 22:07:17 -0400

    Update vite.config.demo.js

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: '/hdf5-as-virtual-zarr/',
+  base: '/hdf5-as-virtual-zarr.js/',
   resolve: {
     alias: {
       "hdf5-as-virtual-zarr": resolve(import.meta.dirname, "src/index.ts"),
